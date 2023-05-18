beee4ef5c0d701ac50c4fef617de1459925ce148
user404e <e404user@gmail.com>
2023-05-18 07:12:42 +0000

    JAVA

---
+++ MEDICAL STORE MANAGEMENT SYSTEM/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int choice = -1;
+
+        while(choice!=0){
+            System.out.println("1) ADMIN");
+            System.out.println("2) EMPLOYEE");
+            System.out.println("0) Exit\n");
+
+            System.out.print("_ : ");
+
+            choice = scan.nextInt();
+            
+        }
+    }
+}
