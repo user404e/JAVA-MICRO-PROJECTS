97cc00e59e358fe0e338a30081a9c964d3152bff
user404e <e404user@gmail.com>
2023-05-21 14:48:49 +0000

    JAVA

--- MEDICAL STORE MANAGEMENT SYSTEM/Main.java
+++ MEDICAL STORE MANAGEMENT SYSTEM/Main.java
@@ -4,17 +4,41 @@ public class Main{
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
         int choice = -1;
-
+        
         while(choice!=0){
-            System.out.println("1) ADMIN");
+            System.out.println("\n1) ADMIN");
             System.out.println("2) STAFF");
             System.out.println("0) Exit\n");
 
             System.out.print("_ : ");
 
             choice = scan.nextInt();   
+
+            if(choice == 1){
+                login l1 = new login();
+
+                int count = 1;
+
+                while(count<=3){
+                    System.out.println("\n!- ADMIN LOGIN -!");
+                    System.out.print("\nUserID : ");
+                    String id = scan.next();
+
+                    System.out.print("Password : ");
+                    String pass = scan.next();
+
+                    if(l1.loginadmin(id, pass)){
+                        System.out.println("Login Successful");
+                        break;
+                    }
+                    else{
+                        System.out.println("\nWRONG ID PASS , TRY AGAIN");
+                        System.out.println((3-count) + " CHANCES LEFT - ");
+                        count ++;
+                    }
+                }
+            }
         }
     }
 }
---
+++ MEDICAL STORE MANAGEMENT SYSTEM/login.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class login{
+    boolean loginadmin(String userid,String pass){
+        try{
+            File f = new File("LoginId.csv");
+            Scanner scan = new Scanner(f);
+
+            String idpass = scan.next();
+            String[] idpassarr; 
+
+            idpassarr = idpass.split(",");
+
+            String id = idpassarr[0];
+            String pas = idpassarr[1];
+
+            if(id.equals(userid) && pas.equals(pass)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch(FileNotFoundException e){
+            return false;
+        }
+    }
+}
