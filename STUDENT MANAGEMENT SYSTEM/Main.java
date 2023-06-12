45cd4dbf2863afe3324ad0562940a8be83553d2b
user404e <e404user@gmail.com>
2023-06-12 08:14:19 +0000

    JAVA

--- STUDENT MANAGEMENT SYSTEM/Main.java
+++ STUDENT MANAGEMENT SYSTEM/Main.java
@@ -15,8 +15,7 @@ public class Main {
             
             switch (choice) {
                 case 1:
-                    System.out.println("\n -------- Faculty Menu --------");
-
+                    
                     int count = 1;
                     Login lg = new Login();
 
@@ -30,6 +29,37 @@ public class Main {
                         if(lg.facultylogin(id, pass)){
 
                             System.out.println("Login Successfull");
+                            int choice1 = -1;
+
+                            do {
+                                System.out.println("\n -------- Faculty Menu --------");
+                                System.out.println("\n1 - ADD NEW STUDENT");
+                                System.out.println("2 - REMOVE A STUDENT");
+                                System.out.println("3 - SHOW STUDENT PROFILE");
+                                System.out.println("4 - UPDATE STUDENT PROFILE");
+
+                                System.out.print("\nEnter Your Choice : ");
+                                choice1 = sc.nextInt();
+
+                                if(choice1 == 1){
+                                    System.out.println("\n ---- Add New Student ----");
+                                }
+                                else if(choice1 == 2){
+                                    System.out.println("\n ---- Remove Student ----");
+                                }
+                                else if(choice1 == 3){
+                                    System.out.println("\n ---- Search Student Profile ----");
+                                }
+                                else if(choice1 == 4){
+                                    System.out.println("\n ---- Update Student Profile ----");
+                                }
+                                else if(choice1 == 0){
+                                    System.out.println("\nExited Faculty Menu - ");
+                                }
+                                else{
+                                    System.out.println("\nInvalid Input -");
+                                }
+                            } while (choice!=0);
                             break;
                         }
                         else{
@@ -37,6 +67,8 @@ public class Main {
                             System.out.println((3-count) + " Chances Left");
                         }
                         count ++;
+                        if(count == 3){
+                        }
                     } while (count<=3);
 
                     break;
